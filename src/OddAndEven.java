954d47143497a547cf76c61149467d99f4a7e8eb
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-18 15:01:32 +0300

    Initial commit

---
+++ src/OddAndEven.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class OddAndEven {
+
+    public static void checkIfOddOrEven(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int num = scanner.nextInt();
+        System.out.println(num);
+
+    }
+}
